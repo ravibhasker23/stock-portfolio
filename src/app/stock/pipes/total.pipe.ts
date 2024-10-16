d97c53e70b3cb524aa3dd9350f0e54c769a9fd7c
ravibhasker23 <ravibhasker23@gmail.com>
2024-10-16 11:33:30 +0200

    added comments for total.pipe.ts

--- src/app/stock/pipes/total.pipe.ts
+++ src/app/stock/pipes/total.pipe.ts
@@ -1,3 +1,6 @@
+// This file defines an Angular pipe named 'total'.
+// The pipe is used to calculate the total value of a specific field for an array of stock objects.
+
 import { Pipe, PipeTransform } from '@angular/core';
 import { IStock } from '../store/stock-state.model';
 
@@ -5,6 +8,7 @@ import { IStock } from '../store/stock-state.model';
   name: 'total',
 })
 export class TotalPipe implements PipeTransform {
+  // The transform method takes an array of stocks and a field name
   transform(stocks: IStock[], field: keyof IStock): number {
     return stocks.reduce((total, stock) => total + (stock[field] as number), 0);
   }
