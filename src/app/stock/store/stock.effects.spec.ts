9d4a7b870d8b63f424a8ebf1b45fcb08348cb35a
ravibhasker23 <ravibhasker23@gmail.com>
2024-10-16 12:17:51 +0200

    Update stock.effects.spec.ts

--- src/app/stock/store/stock.effects.spec.ts
+++ src/app/stock/store/stock.effects.spec.ts
@@ -5,12 +5,13 @@ import { StockEffects } from './stock.effects';
 import { StockService } from '../services/stock.service';
 import { ActionTypes, AddStockSuccess, AddStockError } from './stock.actions';
 import { Action } from '@ngrx/store';
-import { cold, hot } from 'jasmine-marbles';
+import { TestScheduler } from 'rxjs/testing';
 
 describe('StockEffects', () => {
   let actions$: Observable<Action>;
   let effects: StockEffects;
   let stockService: jasmine.SpyObj<StockService>;
+  let testScheduler: TestScheduler;
 
   beforeEach(() => {
     const spy = jasmine.createSpyObj('StockService', ['getStocksBySymbol', 'getExchangeRate', 'normaliseToEur']);
@@ -25,56 +26,67 @@ describe('StockEffects', () => {
 
     effects = TestBed.inject(StockEffects);
     stockService = TestBed.inject(StockService) as jasmine.SpyObj<StockService>;
+    testScheduler = new TestScheduler((actual, expected) => {
+      expect(actual).toEqual(expected);
+    });
   });
 
   it('should return AddStockSuccess when adding stocks with .Q symbol', () => {
-    const action = { type: ActionTypes.ADD_STOCKS, payload: { vwdKey: 'AAPL.Q', numberOfContracts: 10, buyValue: 100 } };
-    const stocksResponse = [{ price: 150 }];
-    const rateResponse = [{ price: 1.1 }];
-    const convertedPrice = 136.36; // 150 / 1.1
+    testScheduler.run(({ expectObservable }) => {
+      const action = { type: ActionTypes.ADD_STOCKS, payload: { vwdKey: 'AAPL.Q', numberOfContracts: 10, buyValue: 100 } };
+      const stocksResponse = [{ price: 150 }];
+      const rateResponse = [{ price: 1.1 }];
+      const convertedPrice = 136.36; // 150 / 1.1
 
-    actions$ = hot('-a', { a: action });
-    stockService.getStocksBySymbol.and.returnValue(cold('-a|', { a: stocksResponse }));
-    stockService.getExchangeRate.and.returnValue(cold('-a|', { a: rateResponse }));
-    stockService.normaliseToEur.and.returnValue(convertedPrice);
+      actions$ = of(action);
+      stockService.getStocksBySymbol.and.returnValue(of(stocksResponse));
+      stockService.getExchangeRate.and.returnValue(of(rateResponse));
+      stockService.normaliseToEur.and.returnValue(convertedPrice);
 
-    const expected = cold('---b', { b: new AddStockSuccess({ stock: jasmine.any(Object) }) });
+      const expected = of(new AddStockSuccess({ stock: jasmine.any(Object) }));
 
-    expect(effects.addStocks$).toBeObservable(expected);
+      expectObservable(effects.addStocks$).toBe(expected);
+    });
   });
 
   it('should return AddStockError when adding stocks with .Q symbol fails', () => {
-    const action = { type: ActionTypes.ADD_STOCKS, payload: { vwdKey: 'AAPL.Q', numberOfContracts: 10, buyValue: 100 } };
+    testScheduler.run(({ expectObservable }) => {
+      const action = { type: ActionTypes.ADD_STOCKS, payload: { vwdKey: 'AAPL.Q', numberOfContracts: 10, buyValue: 100 } };
 
-    actions$ = hot('-a', { a: action });
-    stockService.getStocksBySymbol.and.returnValue(throwError('error'));
-    stockService.getExchangeRate.and.returnValue(of([{ price: 1.1 }]));
+      actions$ = of(action);
+      stockService.getStocksBySymbol.and.returnValue(throwError('error'));
+      stockService.getExchangeRate.and.returnValue(of([{ price: 1.1 }]));
 
-    const expected = cold('-b', { b: new AddStockError({ errorCode: 1, errorMsg: 'Unable to add the stock to the portfolio' }) });
+      const expected = of(new AddStockError({ errorCode: 1, errorMsg: 'Unable to add the stock to the portfolio' }));
 
-    expect(effects.addStocks$).toBeObservable(expected);
+      expectObservable(effects.addStocks$).toBe(expected);
+    });
   });
 
   it('should return AddStockSuccess when adding stocks without .Q symbol', () => {
-    const action = { type: ActionTypes.ADD_STOCKS, payload: { vwdKey: 'AAPL', numberOfContracts: 10, buyValue: 100 } };
-    const stocksResponse = [{ price: 150 }];
+    testScheduler.run(({ expectObservable }) => {
+      const action = { type: ActionTypes.ADD_STOCKS, payload: { vwdKey: 'AAPL', numberOfContracts: 10, buyValue: 100 } };
+      const stocksResponse = [{ price: 150 }];
 
-    actions$ = hot('-a', { a: action });
-    stockService.getStocksBySymbol.and.returnValue(cold('-a|', { a: stocksResponse }));
+      actions$ = of(action);
+      stockService.getStocksBySymbol.and.returnValue(of(stocksResponse));
 
-    const expected = cold('--b', { b: new AddStockSuccess({ stock: jasmine.any(Object) }) });
+      const expected = of(new AddStockSuccess({ stock: jasmine.any(Object) }));
 
-    expect(effects.addStocks$).toBeObservable(expected);
+      expectObservable(effects.addStocks$).toBe(expected);
+    });
   });
 
   it('should return AddStockError when adding stocks without .Q symbol fails', () => {
-    const action = { type: ActionTypes.ADD_STOCKS, payload: { vwdKey: 'AAPL', numberOfContracts: 10, buyValue: 100 } };
+    testScheduler.run(({ expectObservable }) => {
+      const action = { type: ActionTypes.ADD_STOCKS, payload: { vwdKey: 'AAPL', numberOfContracts: 10, buyValue: 100 } };
 
-    actions$ = hot('-a', { a: action });
-    stockService.getStocksBySymbol.and.returnValue(throwError('error'));
+      actions$ = of(action);
+      stockService.getStocksBySymbol.and.returnValue(throwError('error'));
 
-    const expected = cold('-b', { b: new AddStockError({ errorCode: 1, errorMsg: 'Unable to add the stock to the portfolio' }) });
+      const expected = of(new AddStockError({ errorCode: 1, errorMsg: 'Unable to add the stock to the portfolio' }));
 
-    expect(effects.addStocks$).toBeObservable(expected);
+      expectObservable(effects.addStocks$).toBe(expected);
+    });
   });
 });
