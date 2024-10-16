b6eaa8797f64adb765f5472342231e6f89ed11ea
ravibhasker23 <ravibhasker23@gmail.com>
2024-10-16 11:39:34 +0200

    Update unit test case for stock-portfolio-view.component.spec.ts

--- src/app/stock/components/stock-portfolio-view/stock-portfolio-view.component.spec.ts
+++ src/app/stock/components/stock-portfolio-view/stock-portfolio-view.component.spec.ts
@@ -1,22 +1,75 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component, Input, OnChanges } from '@angular/core';
+import { IStock } from '../../store/stock-state.model';
+import * as Highcharts from 'highcharts';
+import { TotalPipe } from '../../pipes/total.pipe';
 
-import { StockPortfolioViewComponent } from './stock-portfolio-view.component';
+@Component({
+  selector: 'app-stock-portfolio-view',
+  templateUrl: './stock-portfolio-view.component.html',
+  styleUrl: './stock-portfolio-view.component.scss',
+  providers: [TotalPipe],
+})
+export class StockPortfolioViewComponent implements OnChanges {
+  @Input()
+  stocks!: IStock[];
 
-describe('StockPortfolioViewComponent', () => {
-  let component: StockPortfolioViewComponent;
-  let fixture: ComponentFixture<StockPortfolioViewComponent>;
+  Highcharts: typeof Highcharts = Highcharts;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [StockPortfolioViewComponent],
-    }).compileComponents();
+  chartOptions: Highcharts.Options = {};
 
-    fixture = TestBed.createComponent(StockPortfolioViewComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  currentChartOptions: Highcharts.Options = {};
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+  constructor(private totalPipe: TotalPipe) {}
+
+  ngOnChanges(): void {
+    this.updateChart();
+  }
+
+  updateChart() {
+    const boughtData = this.stocks.map((el) => ({
+      name: el.name,
+      y: Number(el.buyValue.toFixed(2)),
+    }));
+
+    const currentData = this.stocks.map((el) => ({
+      name: el.name,
+      y: Number(el.currentValue.toFixed(2)),
+    }));
+
+    let data = boughtData;
+
+    this.chartOptions = {
+      chart: {
+        type: 'pie',
+      },
+      title: {
+        text: 'Bought values',
+      },
+      series: [
+        {
+          name: 'Bought',
+          data,
+          type: 'pie',
+        },
+      ],
+    };
+
+    data = currentData;
+
+    this.currentChartOptions = {
+      chart: {
+        type: 'pie',
+      },
+      title: {
+        text: 'Current values',
+      },
+      series: [
+        {
+          name: 'Current',
+          data,
+          type: 'pie',
+        },
+      ],
+    };
+  }
+}
